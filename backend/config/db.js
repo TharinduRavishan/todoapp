b1895006e92d1cb03d8b41937291378656b21a96
Sudarshana Dayananda <sudarshana@sourcecodely.com>
2021-05-22 23:52:39 +0530

    fake user

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,25 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import User from '../api/resources/user/user.model';
 
 mongoose.Promise = global.Promise;
 
 export default {
+  connect() {
+    mongoose.connect('mongodb://localhost/my_todo', { useNewUrlParser: true });
+    this.createFakeAdmin();
+  },
 
-    connect() {
-        mongoose.connect('mongodb://localhost/my_todo', { useNewUrlParser: true });
+  async createFakeAdmin() {
+    const admin = {
+      email: 'admin@gmail.com',
+      password: await bcrypt.hash('admin', 10),
+      firstName: 'Admin',
+      lastName: 'User',
+    };
+    const found = await User.find({ email: 'admin@gmail.com' });
+    if (found.length === 0) {
+      await User.create(admin);
     }
-}
+  },
+};
